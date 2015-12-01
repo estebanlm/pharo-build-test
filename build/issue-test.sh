bb533461ed7336ba79bbaa8a5c1be952dd33ce28
Esteban Lorenzano <estebanlm@gmail.com>
2015-12-01 15:14:18 +0100

    small adjust

--- build/issue-test.sh
+++ build/issue-test.sh
@@ -10,6 +10,10 @@ echo -e "\e[32m======================"
 echo -e "\e[32m= DOWNLOAD RESOURCES ="
 echo -e "\e[32m======================"
 echo -e "\e[0m"
+
+wget --quiet -O - get.pharo.org/50+vm | bash
+
+echo "Downloading support files..."
 wget --quiet \
 	--mirror --no-parent \
     --reject "*.html*" \
@@ -17,8 +21,6 @@ wget --quiet \
     --directory-prefix bootstrap \
     --cut-dirs=2 \
     http://files.pharo.org/extra/bootstrap/
-
-wget --quiet -O - get.pharo.org/50+vm | bash
 
 # PREPARE CI
 echo -e ""
@@ -28,8 +30,6 @@ echo -e "\e[32m=============="
 echo -e "\e[0m"
 ./pharo Pharo.image save ci --delete-old
 ./pharo ci.image config http://smalltalkhub.com/mc/Pharo/ci/main ConfigurationOfCI --install=stable
-echo -e ""
-echo -e "Using CI version: `./pharo ci.image eval "(ConfigurationOfCI project version: #stable) versionNumber"`"
 # Obtain user/password
 export PHARO_CI_USER=`./pharo ci.image eval "IntegrationManager userName" | sed "s/'//g"`
 export PHARO_CI_PWD=`./pharo ci.image eval "IntegrationManager password" | sed "s/'//g"`
@@ -42,8 +42,11 @@ echo -e "\e[32m================="
 echo -e "\e[0m"
 
 if [ -n "$ISSUE" ]; then
+	echo "Validate issue $ISSUE"
 	TO_VALIDATE="--issue=$ISSUE"
 else
+	echo "Validate next issue"	
 	TO_VALIDATE="--update-issue --next"
 fi
+
 ./pharo ci.image ci slice test --html-resources="file://./bootstrap/" $TO_VALIDATE
