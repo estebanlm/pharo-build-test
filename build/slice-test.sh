234382c93da259caba69f26c05a917c481a69457
Esteban Lorenzano <estebanlm@gmail.com>
2015-12-01 11:29:07 +0100

    print CI version

--- build/slice-test.sh
+++ build/slice-test.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 # DOWNLOAD THE EXTERNAL HTML RESOURCES
+echo -e ""
+echo -e "\e[32m======================"
+echo -e "\e[32m= DOWNLOAD RESOURCES ="
+echo -e "\e[32m======================"
+echo -e "\e[0m"
 wget --quiet \
 	--mirror --no-parent \
     --reject "*.html*" \
@@ -12,15 +17,19 @@ wget --quiet \
 wget --quiet -O - get.pharo.org/50+vm | bash
 
 # PREPARE CI
-echo -e "\e[32m================"
-echo -e "\e[32m=  PREPARE CI  ="
-echo -e "\e[32m================"
+echo -e ""
+echo -e "\e[32m=============="
+echo -e "\e[32m= PREPARE CI ="
+echo -e "\e[32m=============="
 echo -e "\e[0m"
 ./pharo Pharo.image save ci --delete-old
 ./pharo ci.image config http://smalltalkhub.com/mc/Pharo/ci/main ConfigurationOfCI --install=stable
+./pharo ci.image eval "(ConfigurationOfCI project version: #stable) versionNumber"
+
 # EXECUTE TESTS
-echo -e "\e[32m==================="
-echo -e "\e[32m=  Execute tests  ="
-echo -e "\e[32m==================="
+echo -e ""
+echo -e "\e[32m================="
+echo -e "\e[32m= EXECUTE TESTS ="
+echo -e "\e[32m================="
 echo -e "\e[0m"
 ./pharo ci.image ci slice test --html-resources="file://./bootstrap/" --update-issue --next
