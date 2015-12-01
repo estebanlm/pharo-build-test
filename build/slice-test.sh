d629244595de73744312cf3fd2876b3a932ecf0a
Esteban Lorenzano <estebanlm@gmail.com>
2015-12-01 11:11:17 +0100

    added quiet added info echoes

--- build/slice-test.sh
+++ build/slice-test.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # DOWNLOAD THE EXTERNAL HTML RESOURCES
-wget --mirror --no-parent \
+wget --quiet \
+	--mirror --no-parent \
     --reject "*.html*" \
     --no-host-directories \
     --directory-prefix bootstrap \
@@ -11,7 +12,9 @@ wget --mirror --no-parent \
 wget --quiet -O - get.pharo.org/50+vm | bash
 
 # PREPARE CI
+echo "Prepare CI"
 ./pharo Pharo.image save ci --delete-old
 ./pharo ci.image config http://smalltalkhub.com/mc/Pharo/ci/main ConfigurationOfCI --install=stable
 # EXECUTE TESTS
+echo "Execute tests"
 ./pharo ci.image ci slice test --html-resources="./bootstrap/" --update-issue --next
